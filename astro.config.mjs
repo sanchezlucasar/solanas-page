677cb87f4b25ef2dabdb1596989175ef9350183b
sanchezlucasar <sanchezlucasar@gmail.com>
2025-09-04 19:56:51 -0300

    1er commit,

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from "node:url";
+
+export default defineConfig({
+    site: 'https://sanchezlucasar.github.io',
+    base: '/solanas-page',
+    outDir: 'dist', // Carpeta donde se genera el build (por defecto "dist")
+    vite: {
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        },
+    },
+});
+
