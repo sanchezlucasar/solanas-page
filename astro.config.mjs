d325feea551a0019ab02e53e9f7c027ef332995a
sanchezlucasar <sanchezlucasar@gmail.com>
2025-09-04 20:29:26 -0300

    update: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { fileURLToPath } from "node:url";
 
 export default defineConfig({
     site: 'https://sanchezlucasar.github.io',
-    base: '/solanas-page',
+    base: '/solanas-page/',
     outDir: 'dist', // Carpeta donde se genera el build (por defecto "dist")
     vite: {
         resolve: {
